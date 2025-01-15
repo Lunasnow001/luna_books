fa75dd003348a4be36534b74843f16d2156d521f
Lunasnow001 <touchnewbiewebsite@gmail.com>
2025-01-15 08:14:47 +0700

    add a validations data

--- app/admin/books/page.tsx
+++ app/admin/books/page.tsx
@@ -1,7 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <section className="bg-white p-7 rounded-2xl w-full">Books</section>;
+  return (
+    <section className="bg-white p-7 rounded-2xl w-full">
+      <div className="flex flex-wrap justify-between items-center gap-2">
+        <h2 className="font-semibold text-xl">All Books</h2>
+        <Button className="bg-primary-admin" asChild>
+          <Link href="/admin/books/new" className="text-white">
+            + Create a New Book
+          </Link>
+        </Button>
+      </div>
+
+      <div className="mt-7 w-full overflow-hidden">
+        <p>Table</p>
+      </div>
+    </section>
+  );
 };
 
 export default page;
